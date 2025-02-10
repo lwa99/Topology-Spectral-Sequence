f110d5b75f2c1246ec53fa79074adfc6645bf11b
Yuankun Zou <Kunko2005@hotmail.com>
2025-02-10 16:18:43 -0500

    Update math.py

--- math.py
+++ math.py
@@ -19,29 +19,31 @@ class PField:
             i += 1
         return True
 
+    def _check_range(self, *args):
+        if PField.debug:
+            for i in args:
+                if i >= self.n:
+                    warn("PField: Input Element Out of Range")
+
     def __init__(self, n: int):
-        if not PField._isprime(n):
+        if PField.debug and not PField._isprime(n):
             raise RuntimeError("PField: Not Prime Order")
         self.n = n
 
     def add(self, e_1: int, e_2: int):
-        if PField.debug and (e_1 >= self.n or e_2 >= self.n):
-            warn("PField: Input Element Out of Range")
+        self._check_range(e_1, e_2)
         return (e_1 + e_2) % self.n
 
     def add_inv(self, e: int):
-        if PField.debug and e >= self.n:
-            warn("PField: Input Element Out of Range")
+        self._check_range(e)
         return (-e) % self.n
 
     def mul(self, e_1: int, e_2: int):
-        if PField.debug and (e_1 >= self.n or e_2 >= self.n):
-            warn("PField: Input Element Out of Range")
+        self._check_range(e_1, e_2)
         return (e_1 * e_2) % self.n
 
     def mul_inv(self, e: int):
-        if PField.debug and e >= self.n:
-            warn("PField: Input Element Out of Range")
+        self._check_range(e)
         return pow(e, -1, self.n)
 
 
@@ -49,6 +51,8 @@ class BiGradedAlgebra:
     class HTerm:
         """
         Homogeneous Term
+
+        Every homogeneous term is a function that maps
         """
 
     class HSpace:
@@ -59,7 +63,9 @@ class BiGradedAlgebra:
     def __init__(self, basis_name: list[str], basis_bigrades: list[tuple[int, int]]):
         self.basis_name = basis_name
         self.basis_bigrades = basis_bigrades
-        self.h_spaces: list[list[BiGradedAlgebra.HSpace]] = []  # This 2-D array is ordered in both direction.
+        self.h_spaces: list[list[BiGradedAlgebra.HSpace]] = []
+        # This 2-D array is ordered in both direction to facilitate further search.
+        # TODO: It might be a better practice to define a class for this
 
     def calculate_bigrade(self):
         pass
