37b97ee0c10ae6cc2df8995ec0237f92e39738fa
Yuankun Zou <Kunko2005@hotmail.com>
2025-02-17 00:45:51 -0500

    Implements some basic functions in Polynomial class

---
+++ Test.py
@@ -0,0 +1,3 @@
+a = [1]
+a.insert(2, 10)
+print(a, len(a))
--- math.py
+++ math.py
@@ -1,11 +1,14 @@
 """
-Implements Finite Prime Field
+Math Structures for the Project
 """
 
 from warnings import warn
 
 
 class PField:
+    """
+    Fintie prime Field of characteristic (and order) n
+    """
     debug = False
 
     @staticmethod
@@ -48,21 +51,75 @@ class PField:
 
 
 class BiGradedAlgebra:
-    class HTerm:
+    class DegreeConfiguration:
+        """
+        A degree configuration is an ordered list of degree on each generator. It is equipped with the dictionary
+        order to speed up searching.
+        """
+        def __init__(self, degree_config: tuple[int]):
+            self.degree_config = degree_config
+
+        def __le__(self, other):
+            for i, n in enumerate(self.degree_config):
+                if n < other.degree_config[i]:
+                    return True
+            return False
+
+        def __ge__(self, other):
+            for i, n in enumerate(self.degree_config):
+                if n > other.degree_config[i]:
+                    return True
+            return False
+
+        def __eq__(self, other):
+            for i, n in enumerate(self.degree_config):
+                if n != other.degree_config[i]:
+                    return False
+            return True
+
+    class Polynomial:
         """
         Homogeneous Term
 
-        Every homogeneous term is a function that maps
+        Every polynomial is a function that maps a degree configuration to an integer?? coefficient.
+        For example, (2, 3) -> 4 means 4x^2y^3 (where x is the first generator and y is the second).
         """
+        def __init__(self, degree_configs: list, coefficients: list):
+            self.coefficient_array = []
+            for n, deg_config in enumerate(degree_configs):
+                self._update(deg_config, coefficients[n])
+
+        def _update(self, deg_config: list, coefficient: int):
+            target_list = self.coefficient_array
+            for k, i in enumerate(deg_config):
+                if i >= len(target_list):
+                    target_list += [None] * (deg_config[k] - len(target_list))
+                    target_list.append([])
+                if k == len(deg_config) - 1:
+                    target_list[i] = coefficient
+                    break
+                target_list = target_list[i]
+
+        def peek(self, deg_config: list) -> int:
+            target_list = self.coefficient_array
+            for k, i in enumerate(deg_config):
+                if k == len(deg_config) - 1:
+                    return target_list[i]
+                target_list = target_list[i]
+
+        def __str__(self):
+            output = ""
+
+            return self.coefficient_array.__str__()
 
     class HSpace:
         """
-        Space of Homogeneous Terms
+        Space of Homogeneous Terms with the same bigrade.
         """
 
-    def __init__(self, basis_name: list[str], basis_bigrades: list[tuple[int, int]]):
-        self.basis_name = basis_name
-        self.basis_bigrades = basis_bigrades
+    def __init__(self, generators: list[str], generator_bigrades: list[tuple[int, int]]):
+        self.generators = generators
+        self.generator_bigrades = generator_bigrades
         self.h_spaces: list[list[BiGradedAlgebra.HSpace]] = []
         # This 2-D array is ordered in both direction to facilitate further search.
         # TODO: It might be a better practice to define a class for this
@@ -72,6 +129,7 @@ class BiGradedAlgebra:
 
 
 if __name__ == "__main__":
-    F = PField(7)
-    print(F.add_inv(4))
-    print(F.mul_inv(4))
+    BA = BiGradedAlgebra(["x", "y"], [(1, 2), (3, 4)])
+    P = BA.Polynomial([[1, 4], [2, 5]], [10, 20])
+    print(P)
+    print(P.peek([2, 5]))
